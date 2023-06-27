a7b0117e15ab3983153994646135c1be90f4dff7
uelionobre <uelionobre@hotmail.com>
2023-06-27 12:33:32 -0300

    adiciona controller para listar partidas em progresso

--- app/backend/src/controllers/matches.controller.ts
+++ app/backend/src/controllers/matches.controller.ts
@@ -1,11 +1,25 @@
 import { Request, Response } from 'express';
 import MatchModel from '../models/MatchModel';
+import MatchesServices from '../services/matches.services';
 
 export default class MatchesController {
   public static async listAll(req: Request, res: Response) {
+    if (req.query.inProgress) {
+      return MatchesController.listProgress(req, res);
+    }
+
     const matchModel = new MatchModel();
     const matches = await matchModel.findAll();
 
-    res.json(matches);
+    return res.json(matches);
+  }
+
+  public static async listProgress(req: Request, res: Response) {
+    const { inProgress } = req.query;
+    const progress = inProgress === 'true' || false;
+
+    const matches = await MatchesServices.listByProgress(progress);
+
+    return res.json(matches);
   }
 }
