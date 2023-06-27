aa043000eab29efae6e66b74825d57561003f135
uelionobre <uelionobre@hotmail.com>
2023-06-27 17:34:38 -0300

    lista progresso dos times

--- app/backend/src/routes/MatchesRouter.ts
+++ app/backend/src/routes/MatchesRouter.ts
@@ -4,5 +4,6 @@ import MatchesController from '../controllers/matches.controller';
 const MatchesRouter = Router();
 
 MatchesRouter.get('/', MatchesController.listAll);
+MatchesRouter.get('/', MatchesController.listProgress);
 
 export default MatchesRouter;
