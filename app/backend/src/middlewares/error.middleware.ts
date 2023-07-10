6f9ef452ea1e7e99cbdf0e3d564a58537003c63b
uelionobre <uelionobre@hotmail.com>
2023-07-10 17:46:21 -0300

    Lanca excecao para Matches nao encontrados ou inexistentes

--- app/backend/src/middlewares/error.middleware.ts
+++ app/backend/src/middlewares/error.middleware.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import LoginErrorException from '../errors/login.error';
 import AuthErrorException from '../errors/auth.error';
+import NotFoundErrorException from '../errors/notFound.error';
 
 export default class ErrorMiddleware {
   static handler(error: Error, _req: Request, res: Response, _next: NextFunction) {
@@ -12,6 +13,10 @@ export default class ErrorMiddleware {
 
     if (error instanceof AuthErrorException) {
       return res.status(401).json({ message });
+    }
+
+    if (error instanceof NotFoundErrorException) {
+      return res.status(404).json({ message });
     }
 
     return res.status(500).json({ message });
